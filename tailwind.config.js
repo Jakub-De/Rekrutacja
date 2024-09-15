16ccf351bde0a92b48e518b4f48bbad68ba62664
Jakub Debski <72356426+Jakub-De@users.noreply.github.com>
2024-09-15 18:24:37 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: '#0147ff',
+        primaryWhite: '#f7f7f7',
+        primaryGray: '#282828',
+      },
+    },
+  },
+  plugins: [],
+}
+
